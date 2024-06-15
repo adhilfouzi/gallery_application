67e272c3ff363a82972476c29482b24e6beeda93
Adhil Fouzi <adhilfouzi@gmail.com>
2024-06-15 11:58:54 +0530

    modifiy design

--- lib/view/album/album_screen.dart
+++ lib/view/album/album_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
@@ -19,70 +18,136 @@ class AlbumScreen extends StatelessWidget {
 
     final imagePath = image.path.toString();
     final stdData = AlbumModel(imagex: imagePath);
-    albumController.addData(stdData);
+    await albumController.addData(stdData);
+    Get.snackbar('Success', 'Image added to album!',
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: Colors.green,
+        colorText: Colors.white);
+  }
+
+  void _showPicker(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext bc) {
+        return SafeArea(
+          child: Wrap(
+            children: <Widget>[
+              ListTile(
+                  leading: const Icon(Icons.photo_library),
+                  title: const Text('Photo Library'),
+                  onTap: () {
+                    getImage(ImageSource.gallery);
+                    Get.back();
+                  }),
+              ListTile(
+                leading: const Icon(Icons.photo_camera),
+                title: const Text('Camera'),
+                onTap: () {
+                  getImage(ImageSource.camera);
+                  Get.back();
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  void _showDeleteConfirmation(BuildContext context, AlbumModel image) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Delete Image'),
+          content: const Text('Are you sure you want to delete this image?'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Delete'),
+              onPressed: () async {
+                albumController.deleteData(image.id!);
+                Get.back();
+                Get.snackbar('Deleted', 'Image has been deleted.',
+                    snackPosition: SnackPosition.BOTTOM,
+                    backgroundColor: Colors.red,
+                    colorText: Colors.white);
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Camera App'), centerTitle: true),
+      appBar: AppBar(
+        title: const Text('My Album'),
+        centerTitle: true,
+      ),
       body: Column(
         children: [
           Expanded(
             child: Obx(() {
               if (albumController.albumimages.isEmpty) {
                 return const Center(
-                    child: Text(
-                  'Album is Empty.....',
-                  style: TextStyle(fontSize: 18),
-                ));
+                  child: Text(
+                    'Album is Empty.....',
+                    style: TextStyle(fontSize: 18, color: Colors.white),
+                  ),
+                );
               } else {
                 return GridView.builder(
+                  padding: const EdgeInsets.all(8.0),
                   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: 3,
+                    crossAxisSpacing: 8.0,
+                    mainAxisSpacing: 8.0,
                   ),
                   itemCount: albumController.albumimages.length,
                   itemBuilder: (BuildContext context, int index) {
                     final pathdata = albumController.albumimages[index];
                     return InkWell(
+                      onTap: () => Get.to(ViewScreen(imageView: pathdata)),
+                      onLongPress: () =>
+                          _showDeleteConfirmation(context, pathdata),
                       child: Card(
                         elevation: 5,
-                        child: Image.file(
-                          File(
-                            pathdata.imagex,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10.0),
+                          child: Image.file(
+                            File(pathdata.imagex),
+                            fit: BoxFit.cover,
+                            errorBuilder: (BuildContext context, Object error,
+                                StackTrace? stackTrace) {
+                              return const Center(
+                                  child: Icon(Icons.broken_image,
+                                      size: 50, color: Colors.grey));
+                            },
                           ),
-                          fit: BoxFit.cover,
                         ),
                       ),
-                      onTap: () => Get.to(ViewScreen(imageView: pathdata)),
                     );
                   },
                 );
               }
             }),
           ),
-          Container(
-            width: double.infinity,
-            color: Colors.black12,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                IconButton(
-                  onPressed: () {
-                    getImage(ImageSource.gallery);
-                  },
-                  icon: const Icon(Icons.photo_album_outlined),
-                ),
-                IconButton(
-                  onPressed: () {
-                    getImage(ImageSource.camera);
-                  },
-                  icon: const Icon(Icons.camera),
-                )
-              ],
-            ),
-          ),
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _showPicker(context),
+        child: const Icon(Icons.add_a_photo),
       ),
     );
   }

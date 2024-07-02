9565761a341f49252f269ec3555af0adfe20184c
Adhil Fouzi <adhilfouzi@gmail.com>
2024-07-02 17:54:48 +0530

    pass

--- lib/model/const/images.dart
+++ lib/model/const/images.dart
@@ -2,4 +2,5 @@ class ImagePath {
   static const String search = 'assets/icons/search.svg';
   static const String moon = 'assets/icons/moon.svg';
   static const String brightness = 'assets/icons/brightness.svg';
+  ///////////////////////////////////
 }
